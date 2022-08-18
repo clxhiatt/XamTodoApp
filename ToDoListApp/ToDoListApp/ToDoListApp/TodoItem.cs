2f740ebb03f8e3230d45cc606a590dfaa66bc398
Chandler Hiatt <chandler.hiatt97@gmail.com>
2022-08-18 12:18:40 -0400

    first

---
+++ ToDoListApp/ToDoListApp/ToDoListApp/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListApp
+{
+    public class TodoItem
+    {
+        public string TodoText { get; set; }
+        public bool Complete { get; set; }
+        public TodoItem(string todoText, bool complete)
+        {
+            this.TodoText = todoText;
+            this.Complete = complete;
+        }
+    }
+}
---
+++ ToDoListApp/ToDoListApp/ToDoListApp/TodoListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoListApp
+{
+    public class TodoListViewModel
+    { 
+
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        
+
+        public TodoListViewModel()
+        {
+            this.TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("Walk The Dog", true));
+            TodoItems.Add(new TodoItem("Take Out The Trash", false));
+            TodoItems.Add(new TodoItem("Clean The Kitchen", false));
+        }
+
+        public string NewTodoInputValue { get; set; }
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem TodoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(TodoItemBeingRemoved);
+        }
+
+
+    }
+
+}
